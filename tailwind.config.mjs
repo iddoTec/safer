e6ee3db7e0bd558203d9124f524eecb717d78402
RobertCoderDev <roberto.smusic.1@gmail.com>
2024-06-19 20:48:51 -0600

    se agrego el hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/preline/dist/*.js',],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				safer: {
+					'50': '#fdf4f3',
+					'100': '#fde5e3',
+					'200': '#fbd0cd',
+					'300': '#f7afaa',
+					'400': '#f18078',
+					'500': '#e6574d',
+					'600': '#d23a30',
+					'700': '#b12d24',
+					'800': '#8f2821',
+					'900': '#7a2822',
+					'950': '#42100d',
+				}
+			}
+		}
 	},
-	plugins: [],
+	plugins: [
+		require('preline/plugin'),
+		require('tailwindcss-animated')
+	]
 }
